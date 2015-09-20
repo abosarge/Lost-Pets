22d2b96fe22214c59a224641968c3eb9519b260e
Mira-M <mira.mollar@gmail.com>
2015-09-19 21:17:46 -0400

    Updated Database connection and SPCA html scrape.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var Firebase = require('firebase');
+
+var myFirebaseRef = new Firebase("https://radiant-inferno-7526.firebaseio.com/");
+var petRef = myFirebaseRef.child("pets");
 
 app.get('/scrape', function(req, res){
 
@@ -10,39 +14,57 @@ app.get('/scrape', function(req, res){
 	var json = new Array();
 	var names = new Array();
 	var ids = new Array();
+	var count;
 
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
 			var pet_name, pet_id;
-			var data = null;
+			var data;
 
-			$('.pets').each(function(i, obj) {
-				$('.pets').filter(function(){
-					data = $(this);
+			$('.pet__heading').each(function(i, obj) {
+				data = $(this);
 
-					names.push(data.find('.pet__heading').text());
-					console.log(data.find('.pet__heading').text());
+				pet_name = data.text();
+				pet_name = pet_name.replace(/[\n\t\r]/g,"");
 
-					ids.push(data.find('.pet__id').text());
-					console.log(data.find('.pet__id').text());
-				})
+				console.log(pet_name);
+				names.push(pet_name);
 			});
 
-			console.log(names[count].trim().substring(10));
+			$('.pet__id').each(function(i, obj) {
+				data = $(this);
 
-			var count = 0;
-			names.forEach(function(entry) {
-				ids.forEach(function(entry) {
-    				json.push({ "pet_name" : names[count].trim().substring(10) , "pet_id" : ids[count].trim().substring(8)});
+				pet_id = data.text();
+				pet_id = pet_id.replace(/[\n\t\r]/g,"");
+
+				console.log(pet_id);
+				ids.push(pet_id);
+			});
+
+			console.log(names);
+			console.log(ids);
+			count = names.length - 1;
+
+			while (count > 0 )
+			{
+				petRef.push({
+					petID: ids[count].substring(8),
+					name: names[count].substring(10),
+					description : "Super fluffy",
+					status : "found",
+					location: {
+						lat : "39.2833",
+						long : "-76.6167",
+						city: "Baltimore",
+						state: "Maryland",
+						zip: "21223"
+					}
 				});
-				count = count + 1;
-			});
-
-			console.log(json);
+				count = count - 1;
+			}
 		}
-		fs.writeFile('output.json', JSON.stringify(json), function(err){
-        	console.log('File successfully written! - Check your project directory for the output.json file');
+        	console.log('File successfully written!');
         })
         res.send('Check your console!')
 	})
